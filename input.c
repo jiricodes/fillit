a5b98ec89e6fae47a772f30e5069f13d4c08a80b
Crowbcat <solopovminer@gmail.com>
2019-10-31 11:30:50 +0200

    it's able to read and check the input

--- input.c
+++ input.c
@@ -6,26 +6,38 @@
 /*   By: asolopov <asolopov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 16:29:11 by asolopov          #+#    #+#             */
-/*   Updated: 2019/10/30 18:48:45 by asolopov         ###   ########.fr       */
+/*   Updated: 2019/10/31 11:30:05 by asolopov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <stdio.h>
 
+int		get_coords(char *tetro);
+
+int		check_neighbour(char *tetro);
+
 int		check_input(char *tetro)
 {
 	int		x;
 	int		len;
+	int		lncnt;
 
+	lncnt = 0;
 	len = ft_strlen(tetro);
 	x = 0;
 	while (tetro[x] != '\0')
 	{
 		if (tetro[x] != '.' && tetro[x] != '#' && tetro[x] != '\n')
 			return (-1);
+		if (x != 20 && tetro[x] == '\n' && x % 5 != 4)
+			return (-1);
+		if (tetro[x] == '\n' || tetro[x + 1] == '\0')
+			lncnt++;
 		x++;
 	}
+	if (lncnt != 4)
+		return (-1);
 	if (len == 21 && tetro[len - 1] != '\n')
 	{
 		return (-1);
@@ -35,25 +47,51 @@ int		check_input(char *tetro)
 
 }
 
+char	*strip_tetro(char *tetro)
+{
+	int	len;
+	int ln_cnt;
+	int x;
+
+	len = ft_strlen(tetro);
+	ln_cnt = 0;
+	x = 0;
+	while (tetro[x] != '\0')
+	{
+		if (tetro[x] == '\n')
+			ln_cnt++;
+		x++;
+	}
+	if (ln_cnt == 5)
+		return (ft_strsub(tetro, 0, len - 1));
+	else
+		return (tetro);
+	
+}
+
 int		get_input(char **argv)
 {
 	char	*buf;
 	int		fd;
 	int		ret;
 	int		x;
-	char	**tetro;
 
 	x = 0;
-	tetro = (char **)malloc(26 * sizeof(char*));
 	buf = (char *)malloc(22 * sizeof(char));
 	fd = open(argv[1], O_RDONLY);
 	while ((ret = read(fd, buf, 21)) > 0)
 	{
 		buf[ret] = '\0';
-		tetro[x] = buf;
-		printf("Tetromino %d is:\n%s", x, tetro[x]);
-		if ((check_input(tetro[x])) != 1)
+		printf("Tetromino %d is:\n%s", x, buf);
+		buf = strip_tetro(buf);
+		printf("!NEW! Tetromino %d is:\n%s", x, buf);
+		if ((check_input(buf)) != 1)
+		{
 			printf("Input is shit");
+			return (-1);
+		}
+		// else
+		// 	get_coords(buf);
 		x++;
 	}
 }
