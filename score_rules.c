7e2e1896667e4cdd8552b165c0ea0845ef73044a
jnovotny <jnovotny@student.hive.fi>
2019-11-06 12:02:43 +0200

    fixed map_score bulshit

--- score_rules.c
+++ score_rules.c
@@ -6,7 +6,7 @@
 /*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/02 22:06:41 by jnovotny          #+#    #+#             */
-/*   Updated: 2019/11/06 10:45:06 by jnovotny         ###   ########.fr       */
+/*   Updated: 2019/11/06 12:02:05 by jnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,18 @@ int		map_score(t_bmap *map)
 	int x;
 	int y;
 
+	x = 0;
+	y = 0;
+	i = 0;
+	res = 0;
 	while (i < MS * MS)
 	{
 		x = i / MS;
 		y = i % MS;
-		res = rule_one(map, x, y) + rule_two(map, x, y) + rule_three(map, x, y);
+		res = res + rule_one(map, x, y) + rule_two(map, x, y) + rule_three(map, x, y);
+		i = i + 1;
 	}
+	printf("Score %d\n", res);
 	return (res);
 }
 
@@ -85,6 +91,6 @@ int		rule_three(t_bmap *map, int i, int j)
 	res = 0;
 	if ((j < map->size - 1) && is_bit_set(map->lines[i], j) \
 			&& (!is_bit_set(map->lines[i], j + 1)))
-		res = res + ((MS - i + 1) * (MS - i + 1));
+		res = res + ((MS - i) * (MS - i));
 	return (res);
 }
